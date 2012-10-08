7756c6add6a3d81a3ca5a53ee294bf7b1a9409bf
Xing Zhou <xing@xingzhou.me>
2012-10-08 11:10:42 -0400

    Debug buildpack

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -577,7 +577,7 @@ params = CGI.parse(uri.query || "")
 
   def generate_jekyll_site
     puts "Building jekyll site"
-    run("env PATH=$PATH bundle exec jekyll --no-server")
+    run("env PATH=$PATH bundle exec jekyll --no-server 2>&1")
     unless $? == 0
       error "Failed to generate site with jekyll."
     end
